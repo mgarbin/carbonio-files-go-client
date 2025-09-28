72058cf140fdbb380ff3510f4847da22c1a8cee6
DocGarbo <manuel.garbin@gmail.com>
2025-09-28 17:07:46 +0200

    chore: complete download endpoint

--- cmd/carbonio-files-go-client/main.go
+++ cmd/carbonio-files-go-client/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"sync"
 
 	"gopkg.in/yaml.v3"
 )
@@ -42,7 +43,7 @@ func printAllFlags() {
 	})
 }
 
-func recursiveNode(graphqlAuthenticator *graphql.GraphQLAuthenticator, id string, level int) {
+func recursiveListNode(graphqlAuthenticator *graphql.GraphQLAuthenticator, id string, level int) {
 	nodes, nodesErr := graphqlAuthenticator.GetAllNode(id, "NAME_ASC", nil, nil)
 	if nodesErr != nil {
 		panic(nodesErr)
@@ -60,7 +61,7 @@ func recursiveNode(graphqlAuthenticator *graphql.GraphQLAuthenticator, id string
 		fmt.Printf("%s|", z)
 		if child.Type == "FOLDER" {
 			fmt.Printf("%s (%s) \n", child.Name, child.Type)
-			recursiveNode(graphqlAuthenticator, child.ID, level+1)
+			recursiveListNode(graphqlAuthenticator, child.ID, level+1)
 		} else {
 			if child.Extension != nil {
 				fmt.Printf("%s.%s (%s) \n", child.Name, *child.Extension, child.Type)
@@ -69,6 +70,75 @@ func recursiveNode(graphqlAuthenticator *graphql.GraphQLAuthenticator, id string
 			}
 		}
 	}
+}
+
+func createFolder(path string) error {
+	err := os.Mkdir(path, 0755)
+	if err != nil {
+		if os.IsExist(err) {
+			// Folder already exists, skip
+			fmt.Errorf("folder already exist error: %w", err)
+			return nil
+		}
+		// Other error, return it
+		return err
+	}
+	return nil
+}
+
+func recursiveFileDownloader(graphqlAuthenticator *graphql.GraphQLAuthenticator, carbonio *carbonio.HTTPAuthenticator, id, folderPath string) {
+	nodes, nodesErr := graphqlAuthenticator.GetAllNode(id, "NAME_ASC", nil, nil)
+	if nodesErr != nil {
+		panic(nodesErr)
+	}
+
+	maxRetries := 3
+
+	var wg sync.WaitGroup
+	sem := make(chan struct{}, 1) // max 1 goroutines
+
+	for _, child := range nodes {
+		if child.Type == "FOLDER" {
+			folderPath := folderPath + "/" + child.Name
+			//fmt.Printf(folderPath + "\n")
+			err := createFolder(folderPath)
+			if err != nil {
+				fmt.Errorf("folder create error: %w", err)
+			}
+			recursiveFileDownloader(graphqlAuthenticator, carbonio, child.ID, folderPath)
+		} else {
+			if child.Extension != nil {
+				fileName := child.Name + "." + *child.Extension
+				wg.Add(1)
+				sem <- struct{}{} // acquire semaphore slot
+				go func() {
+					exitStat, downErr := carbonio.DownloadFile(graphqlAuthenticator.AuthToken, child.ID, folderPath, fileName, int64(*child.Size), maxRetries, &wg, sem)
+					if downErr != nil {
+						fmt.Printf("[ERROR] %s - ", downErr)
+					}
+					if exitStat != nil {
+						fmt.Printf("[INFO] %s - ", *exitStat)
+					}
+					fmt.Printf("%s/%s.%s\n", folderPath, child.Name, *child.Extension)
+				}()
+			} else {
+				wg.Add(1)
+				sem <- struct{}{} // acquire semaphore slot
+				go func() {
+					exitStat, downErr := carbonio.DownloadFile(graphqlAuthenticator.AuthToken, child.ID, folderPath, child.Name, int64(*child.Size), maxRetries, &wg, sem)
+					if downErr != nil {
+						fmt.Printf("[ERROR] %s - ", downErr)
+					}
+					if exitStat != nil {
+						fmt.Printf("[INFO] %s - ", *exitStat)
+					}
+					fmt.Printf("%s/%s\n", folderPath, child.Name)
+				}()
+			}
+		}
+	}
+
+	wg.Wait()
 }
 
 func main() {
@@ -81,9 +151,10 @@ func main() {
 	var zmAuthToken *string
 	zmAuthToken = cfg.Main.AuthToken
 
+	carbonio := &carbonio.HTTPAuthenticator{Endpoint: cfg.Main.Endpoint}
+
 	if zmAuthToken == nil {
 
-		carbonio := &carbonio.HTTPAuthenticator{Endpoint: cfg.Main.Endpoint}
 		carbonioToken, errCarbonioToken := carbonio.CarbonioZxAuth(cfg.Main.Username, cfg.Main.Password)
 
 		if errCarbonioToken != nil {
@@ -98,9 +169,9 @@ func main() {
 	}
 
 	listAllNode := flag.Bool("getAllNode", false, "Use this flag to obtain all files node")
+	downloadAllFiles := flag.Bool("downloadAllFiles", false, "Use this flag to create Folder directory tree and download all files")
 	printFlagInfo := flag.Bool("v", false, "output helper with all flags")
 
-	// Parse the flags
 	flag.Parse()
 
 	if *printFlagInfo {
@@ -108,30 +179,16 @@ func main() {
 	}
 
 	if *listAllNode {
-		//fmt.Println("ZM_AUTH_TOKEN obatined :", zmAuthToken)
-		//fmt.Println("---------")
 		fmt.Println("Here all nodes found with graphl query!")
 		graphqlAuthenticator := &graphql.GraphQLAuthenticator{Endpoint: cfg.Main.Endpoint, AuthToken: *zmAuthToken}
 		base_folder := "LOCAL_ROOT"
-		recursiveNode(graphqlAuthenticator, base_folder, 0)
-		/*nodes, nodesErr := graphqlAuthenticator.GetAllNode(base_folder, "NAME_ASC", nil, nil)
-		if nodesErr != nil {
-			panic(nodesErr)
-		}
-		recursiveNode(graphqlAuthenticator, base_folder, 1)
+		recursiveListNode(graphqlAuthenticator, base_folder, 0)
+	}
 
-		for _, child := range nodes {
-			if child.Type == "FOLDER" {
-				fmt.Printf("%s (%s) \n", child.Name, child.Type)
-				recursiveNode(graphqlAuthenticator, child.ID, 1)
-			} else {
-				if child.Extension != nil {
-					fmt.Printf("%s.%s (%s) \n", child.Name, *child.Extension, child.Type)
-				} else {
-					fmt.Printf("%s (%s) \n", child.Name, child.Type)
-				}
-			}
-		}*/
+	if *downloadAllFiles {
+		graphqlAuthenticator := &graphql.GraphQLAuthenticator{Endpoint: cfg.Main.Endpoint, AuthToken: *zmAuthToken}
+		base_folder := "LOCAL_ROOT"
+		recursiveFileDownloader(graphqlAuthenticator, carbonio, base_folder, "files")
 	}
 
 	//obatin hash file openssl sha384 -binary /tmp/Rec_2025-09-09_1512_Hyperion_meeting_Agosto_2025_upgrade_25.6.webm | base64
---
+++ pkg/carbonio/carbonio.go
@@ -0,0 +1,265 @@
+package carbonio
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"net/mail"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+)
+
+type Authenticator interface {
+	// Authenticate performs an HTTP POST with email and password,
+	// returns the value of the ZM_AUTH_TOKEN cookie if authentication succeeds.
+	CarbonioZxAuth(email, password string) (string, error)
+	DownloadFile(token, nodeId, destPath string, fileSize int64, maxRetries int) error
+}
+
+type HTTPAuthenticator struct {
+	Endpoint string
+}
+
+// customTransport adds the Cookie header to every request
+type customTransport struct {
+	base                  http.RoundTripper
+	DialContext           *net.Dialer
+	TLSClientConfig       *tls.Config
+	DisableKeepAlives     bool
+	MaxIdleConns          int
+	IdleConnTimeout       int
+	ExpectContinueTimeout time.Duration
+	authToken             string
+}
+
+// ProgressWriter wraps an io.Writer and displays progress.
+type ProgressWriter struct {
+	Writer      io.Writer
+	Total       int64 // expected size
+	Downloaded  int64 // bytes written
+	LastPrinted int64
+	FileName    string
+}
+
+func (pw *ProgressWriter) Write(p []byte) (n int, err error) {
+	n, err = pw.Writer.Write(p)
+	pw.Downloaded += int64(n)
+	// Print every 1% or when finished
+	percent := int(float64(pw.Downloaded) / float64(pw.Total) * 100)
+	lastPercent := int(float64(pw.LastPrinted) / float64(pw.Total) * 100)
+	if pw.Total > 0 && (percent > lastPercent || pw.Downloaded == pw.Total) {
+		fmt.Printf("\r%s: [%-50s] %3d%%", pw.FileName, progressBar(percent), percent)
+		pw.LastPrinted = pw.Downloaded
+		if pw.Downloaded == pw.Total {
+			fmt.Println()
+		}
+	}
+	return
+}
+
+func progressBar(percent int) string {
+	bars := percent / 2
+	return fmt.Sprintf("%s%s", stringRepeat("=", bars), stringRepeat(" ", 50-bars))
+}
+
+func stringRepeat(s string, count int) string {
+	res := ""
+	for i := 0; i < count; i++ {
+		res += s
+	}
+	return res
+}
+
+func (t *customTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	cookieValue := fmt.Sprintf("ZM_AUTH_TOKEN=%s", t.authToken)
+	req.Header.Set("Cookie", cookieValue)
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+	req.Header.Set("Accept-Encoding", "gzip, deflate, br, zstd")
+	req.Header.Set("Sec-Fetch-Dest", "document")
+	req.Header.Set("Sec-Fetch-Mode", "navigate")
+	req.Header.Set("Sec-Fetch-Site", "same-origin")
+	req.Header.Set("Sec-Fetch-User", "?1")
+	req.Header.Set("Priority", "u=0, i")
+	req.Header.Set("Pragma", "no-cache")
+	req.Header.Set("Cache-Control", "no-cache")
+	req.Header.Set("TE", "trailers")
+	return t.base.RoundTrip(req)
+}
+
+func isValidEmail(email string) bool {
+	_, err := mail.ParseAddress(email)
+	return err == nil
+}
+
+func (a *HTTPAuthenticator) CarbonioZxAuth(email, password string) (*string, error) {
+	// Verify if email respect rfc
+	if !isValidEmail(email) {
+		return nil, errors.New("invalid email address format")
+	}
+
+	// Create payload to inject to zx auth endpoint
+	payload := map[string]string{
+		"auth_method": "password",
+		"user":        email,
+		"password":    password,
+	}
+
+	body, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	// Make the request
+	req, err := http.NewRequest("POST", "https://"+a.Endpoint+"/zx/auth/v2/login", bytes.NewBuffer(body))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	// Create HTTP client with SSL verification disabled
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+		Timeout: 5 * time.Second,
+	}
+
+	// Wait for response
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	// Read cookies
+	for _, cookie := range resp.Cookies() {
+		if cookie.Name == "ZM_AUTH_TOKEN" {
+			return &cookie.Value, nil
+		}
+	}
+
+	return nil, errors.New("ZM_AUTH_TOKEN cookie not found")
+}
+
+func (a *HTTPAuthenticator) DownloadFile(token, nodeId, destPath, fileName string, fileSize int64, maxRetries int, wg *sync.WaitGroup, sem chan struct{}) (*string, error) {
+
+	dialer := &net.Dialer{
+		Timeout: 5 * time.Second, // Only dial (connection) timeout
+	}
+
+	skipInsecure := &tls.Config{InsecureSkipVerify: true}
+
+	// Optionally, set up an authenticated HTTP client
+	httpClient := &http.Client{
+		Transport: &customTransport{
+			DialContext:           dialer,
+			TLSClientConfig:       skipInsecure,
+			DisableKeepAlives:     false,
+			MaxIdleConns:          0,
+			IdleConnTimeout:       0,
+			ExpectContinueTimeout: 1 * time.Second,
+			base:                  http.DefaultTransport,
+			authToken:             token,
+		},
+	}
+
+	defer func() {
+		<-sem // release semaphore
+		wg.Done()
+	}()
+
+	var lastErr error
+
+	for attempt := 1; attempt <= maxRetries; attempt++ {
+
+		// Make the request
+		resp, err := httpClient.Get("https://" + a.Endpoint + "/services/files/download/" + nodeId)
+		if err != nil {
+			lastErr = fmt.Errorf("attempt %d: failed to create request: %w", attempt, err)
+			time.Sleep(2 * time.Second)
+			continue
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			lastErr = fmt.Errorf("attempt %d: bad status: %s", attempt, resp.Status)
+			time.Sleep(2 * time.Second)
+			continue
+		}
+
+		// Get content length from header (optional)
+		contentLengthStr := resp.Header.Get("Content-Length")
+		var expectedSize int64 = -1
+		if contentLengthStr != "" {
+			expectedSize, err = strconv.ParseInt(contentLengthStr, 10, 64)
+			if err != nil {
+				// If Content-Length is invalid, just ignore size check
+				expectedSize = -1
+			}
+		}
+
+		if expectedSize != fileSize {
+			lastErr = fmt.Errorf("attempt %d: download files size mistmatch!", attempt)
+			time.Sleep(2 * time.Second)
+			continue
+		}
+
+		info, err := os.Stat(destPath + "/" + fileName)
+		if err == nil {
+			if info.Mode().IsRegular() && info.Size() == expectedSize {
+				//if file already exist go out!
+				exitStatus := "File already exist!"
+				resp.Body.Close()
+				return &exitStatus, nil
+			}
+		}
+
+		out, err := os.Create(destPath + "/" + fileName)
+		if err != nil {
+			lastErr = fmt.Errorf("attempt %d: file create error: %w", attempt, err)
+			time.Sleep(2 * time.Second)
+			continue
+		}
+
+		pw := &ProgressWriter{
+			Writer:   out,
+			Total:    expectedSize,
+			FileName: fileName,
+		}
+		var written int64
+		if expectedSize > 0 {
+			written, err = io.Copy(pw, resp.Body)
+		} else {
+			written, err = io.Copy(out, resp.Body)
+		}
+		out.Close()
+		resp.Body.Close()
+
+		if err != nil {
+			lastErr = fmt.Errorf("attempt %d: file write error: %w", attempt, err)
+			time.Sleep(2 * time.Second)
+			continue
+		}
+
+		if expectedSize >= 0 && written != expectedSize {
+			lastErr = fmt.Errorf("attempt %d: file size mismatch: expected %d bytes, got %d bytes", attempt, expectedSize, written)
+			time.Sleep(2 * time.Second)
+			continue
+		}
+
+		exitStatus := "File downloaded successfully."
+		time.Sleep(1 * time.Second)
+		return &exitStatus, nil
+	}
+
+	fmt.Println("Error creating file: %s \n", lastErr)
+	return nil, lastErr
+
+}
